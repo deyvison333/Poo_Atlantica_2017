963361ecc1329a174608f6efe69ffe5c9263aaa6
deyvison333 <deyvison333@gmail.com>
2018-03-20 12:30:11 +0000

    takeoff

---
+++ TakeOff.java
@@ -0,0 +1,21 @@
+public class TakeOff {
+    public static void main(String[] args) {
+      System.out.println("Iniciando o Nivel 2 - Take Off!");
+
+      int soma = 5+5;
+      int sub = 5-5;
+      int divi = 5/5;
+      int multi = 5*5;
+      int resto = 5 % 2;
+
+      System.out.println("Operadores ariméticos \n");
+      System.out.print("5+5 é igual a: "+ soma +"\n");
+      System.out.print("5-5 é igual a: "+ sub +"\n");
+      System.out.print("5/5 é igual a: "+ divi +"\n");
+      System.out.print("5*5 é igual a: "+ multi +"\n");
+      System.out.print("Resto da divisão de 5 por 2 é igual a: "+ resto +"\n");
+
+      System.out.println("Iniciando o Nivel 2 - Take Off!");
+
+    }
+}
